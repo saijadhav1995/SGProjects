e3d80fd83d75dbe6ec548e11656d2d9380e31621
A5417458 <aparna.kor@ext.saint-gobain.com>
2019-07-03 14:53:41 +0530

    updaed file

--- src/main/java/Utility/AppstringsConstant.java
+++ src/main/java/Utility/AppstringsConstant.java
@@ -12,10 +12,10 @@ public class AppstringsConstant {
 	public final static  String  SUPPLIEREXPECTEDVALIDATIONMANDATORY = "1";
 	public final static  String  SUPPLIEREXPECTEDENTITY = "351";
 	//public final static  String  SUPPLIERPDFPATH = "C:\\Users\\S6670405\\Desktop\\New folder\\Automation\\CAMAL\\Files\\Abc.pdf";
-	public final static  String  SUPPLIERCREATIONMSG = "Fournisseur créé avec succès";
-	public final static  String  SUPPLIERUPDATIONMSG = "Fournisseur mis à jour avec succès";
-	public final static  String  SUPPLIERMAILSENDMSG = "Email envoyé avec succès.";
-	public final static  String  SUPPLIERDELETEMSG   = "Fournisseur supprimé avec succès";
+	public final static  String  SUPPLIERCREATIONMSG = "11Fournisseur crÃ©Ã© avec succÃ¨s";
+	public final static  String  SUPPLIERUPDATIONMSG = "Fournisseur mis Ã  jour avec succÃ¨s";
+	public final static  String  SUPPLIERMAILSENDMSG = "Email envoyÃ© avec succÃ¨s.";
+	public final static  String  SUPPLIERDELETEMSG   = "Fournisseur supprimÃ© avec succÃ¨s";
 	//public final static  String  EDITSUPPLIERPDFPATH = "C:\\Users\\S6670405\\Desktop\\New folder\\Automation\\CAMAL\\Files\\ABCDE.pdf";
 	
 	// Locators object
@@ -27,25 +27,26 @@ public class AppstringsConstant {
 	
 	//User
 	public final static  String  USERROLE = "Administrateur";
-	public final static  String  CREATEUSERMSG = "Utilisateur créé avec succès";
-	public final static  String  EDITUSERMSG = "Utilisateur mis à jour avec succès";
+	public final static  String  CREATEUSERMSG = "Utilisateur crÃ©Ã© avec succÃ¨s";
+	public final static  String  EDITUSERMSG = "Utilisateur mis Ã  jour avec succÃ¨s";
 	
 	//Action Log
-	public final static  String  CREATIONLOG = "Création";
+	public final static  String  CREATIONLOG = "CrÃ©ation";
 	public final static  String  MODIFICATIONLOG = "Modification";
 	public final static  String  DELETIONLOG = "Suppression";
 	public final static  String  USERSGIDLOG = "A5417458";
 	
 	//EmailTab
 	public final static  String  SUPPLIEREMAILCREATION = "//*[@id=\"supplier_type\"]/option";
-	public final static  String MAILCREATIONSUCCESS = "Copie Mail d´information créé avec succès";
-	public final static  String MAILEDITSUCCESS = "Copie Mail d´information mis à jour avec succès";
+	public final static  String MAILCREATIONSUCCESS = "Copie Mail dÂ´information crÃ©Ã© avec succÃ¨s";
+	public final static  String MAILEDITSUCCESS = "Copie Mail dÂ´information mis Ã  jour avec succÃ¨s";
 
 	//Logout
 	public final static  String LOGOUTPAGEHEADER = "LOGIN PAGE";
+	public final static  String INVALIDLOGINMSG = "SSO_002: The Saint-Gobain Identifier or the password is incorrect.";
 	
 	//Excel
-	public static final String EXCELFILEPATH = "/Users/admin/Documents/Caml/automation-projects/";
+	public static final String EXCELFILEPATH = "/Users/admin/WEB/CAMAL/";
 	public static final String EXCELFILENAME= "Test_Data.xlsx";
 	public static final String EXCELLOGINSHEET= "Login";
 }
